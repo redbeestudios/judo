f40507d2939f944349113cd0b8c8f82ad77aa34c
Joaco <joaquin@redb.ee>
2019-05-10 16:31:57 -0300

    improvement(cli): delegate yargs to judo bin

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-const yargs = require('yargs');
 const chalk = require('chalk');
 const cucumber = require('cucumber/lib/cli/run');
-
-const argv = yargs
-    .version(false)
-    .help(false)
-    .usage('Judo')
-    .argv;
-
-const config = require('./runtime/config')(argv);
 
 /**
  *
  * @returns {Promise<void>}
  */
-module.exports.run = async () => {
-
-    console.log(chalk.cyan(`    
-         ▄█ ███    █▄  ████████▄   ▄██████▄  
-        ███ ███    ███ ███   ▀███ ███    ███ 
-        ███ ███    ███ ███    ███ ███    ███ 
-        ███ ███    ███ ███    ███ ███    ███ 
-        ███ ███    ███ ███    ███ ███    ███ 
-        ███ ███    ███ ███    ███ ███    ███ 
-        ███ ███    ███ ███   ▄███ ███    ███ 
-    █▄ ▄███ ████████▀  ████████▀   ▀██████▀  
-    ▀▀▀▀▀▀`));
+module.exports.run = async (options) => {
+    const config = require('./runtime/config')(options);
 
     if (config.judo.sandbox)
         console.log(chalk.red.bold('\nJudo is running in Sandbox mode. All changes will be committed!\n'));
