d60a3ae30d0341ecc9b6ebed676331071ee122fb
lt-mayonesa <juacocampero@gmail.com>
2019-08-19 19:06:30 -0300

    test(steps): select value step

---
+++ __tests__/steps/select-value-step.spec.js
@@ -0,0 +1,28 @@
+const {selectValueStep} = require('../../src/steps');
+
+jest.mock('../../src/engine/operations');
+const operations = require('../../src/engine/operations');
+
+jest.mock('../../src/runtime/transform-data', () => jest.fn(() => 'my_value'));
+
+describe('insert into table step tests', () => {
+
+    test('should insert into table and store inserted values in context $',
+        async () => {
+            spyOn(operations, 'selectValue')
+                .mockImplementation(() => Promise.resolve('MY RESULT'));
+
+            const result = await selectValueStep.call(this, 'my_field', 'my_table', 'my_key', 'my_value');
+
+            expect(this.$my_field).toBe('MY RESULT');
+            expect(result).toBe('MY RESULT');
+        }
+    );
+    test('should propagate the error', () => {
+        spyOn(operations, 'selectValue')
+            .mockImplementation(() => Promise.reject('An error'));
+
+        return selectValueStep('my_field', 'my_table', 'my_key', 'my_value')
+            .catch(e => expect(e).toBe('An error'));
+    });
+});
--- src/steps/given/select-value-step.js
+++ src/steps/given/select-value-step.js
@@ -1,10 +1,18 @@
 const {selectValue} = require('../../engine/operations');
 const transform = require('../../runtime/transform-data');
 
-module.exports = function (field, table, filterBy, value) {
-    return selectValue(field, table, filterBy, transform.call(this, value))
-        .then(value => {
-            this['$' + field] = value;
-            return Promise.resolve(value);
-        });
+/**
+ * Select one value from a table given one criteria only
+ * Useful for having access to database constants during the tests.
+ *
+ *
+ * @param {string} field :: the field to select
+ * @param {string} table :: the table to query
+ * @param {string} filterBy :: the field for the WHERE clause
+ * @param {string} value :: the value for the WHERE clause
+ * @returns {Promise<*>|null} :: the value found
+ */
+module.exports = async function selectValueStep(field, table, filterBy, value) {
+    this['$' + field] = await selectValue(field, table, filterBy, transform.call(this, value));
+    return this['$' + field];
 };
