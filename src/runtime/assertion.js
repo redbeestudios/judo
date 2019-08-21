7778d9d08584c480cb23fb417ddfac76de941fdb
lt-mayonesa <juacocampero@gmail.com>
2019-08-20 23:58:10 -0300

    wip(assertion): replacing chai with custom implementation

--- src/runtime/assertion.js
+++ src/runtime/assertion.js
@@ -2,15 +2,23 @@ const AssertionError = require('assertion-error');
 const deepEqual = require('deep-eql');
 const comparator = require('./loose-comparator');
 
+class JudoError extends AssertionError {
+    constructor(message, props, ssf) {
+        super(message, props, ssf);
+        this.showDiff = true;
+    }
+}
 
 module.exports = function assert(expected, actual) {
     const equals = deepEqual(expected, actual, {comparator: comparator});
 
     if (!equals)
-        throw new AssertionError('Values do not match', {
-            actual,
-            expected
-        });
+        throw new JudoError(
+            `Expected ${expected} but got ${actual}`,
+            {
+                actual,
+                expected
+            }, assert);
 
     return equals;
 };
