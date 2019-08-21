a9c5b5b4326d114425c6af53ecc6449772a75ea2
lt-mayonesa <juacocampero@gmail.com>
2019-08-21 20:55:05 -0300

    fix(run): if any step fails exit with error status code
    changed some tests implementation.

--- __tests__/steps/given/define-variable-step.spec.js
+++ __tests__/steps/given/define-variable-step.spec.js
@@ -1,14 +1,9 @@
 const {defineVariableStep} = require('../../../src/steps');
 
-jest.mock('../../../src/runtime/transform-data');
-const transformMock = require('../../../src/runtime/transform-data');
-
 describe('define variable in test context', () => {
 
     test('should store value in given key',
         () => {
-            transformMock.mockImplementation(() => 'world');
-
             defineVariableStep.call(this, 'world', 'hello');
 
             expect(this.hello).toBe('world');
@@ -17,8 +12,7 @@ describe('define variable in test context', () => {
 
     test('should store transformed value in given key',
         () => {
-            transformMock.mockImplementation(() => 1);
-
+            this.world = 1;
             defineVariableStep.call(this, 'world', 'hello');
 
             expect(this.hello).toBe(1);
